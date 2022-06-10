77818c4476dac55bb0a96f422a0f690048b1ee45
tborba1 <taryn.borba@gmail.com>
2022-06-10 12:15:29 -0700

    feat: updated js to reflect day 7 THC

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 // Day 6 THC
 
-fetch(`https://cat-fact.herokuapp.com/facts/random`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((body) => {
-    console.log(body);
-    alert(body.text);
-  });
+// fetch(`https://cat-fact.herokuapp.com/facts/random`)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((body) => {
+//     console.log(body);
+//     alert(body.text);
+//   });
+
+
+// Day 7 THC
+
+let btn = document.createElement("button");
+btn.innerHTML = "Click here for a random cat fact!";
+document.body.appendChild(btn);
+
+btn.setAttribute("id", "btn");
+
+document.getElementById("btn").addEventListener("click", catFacts);
+
+function catFacts() {
+  fetch(`https://cat-fact.herokuapp.com/facts/random`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((body) => {
+      // console.log(body);
+      // alert(body.text);
+
+      let fact = document.getElementById("randomCat");
+      fact.innerHTML = body.text;
+      document.body.appendChild(fact);
+    });
+}
